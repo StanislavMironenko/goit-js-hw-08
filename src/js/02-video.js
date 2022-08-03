fa6b6ef8f8a8db12a24987d69058e66e5e99624c
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-08-03 15:15:24 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(callback, 1000));
+
+function callback({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
+console.log(JSON.parse(localStorage.getItem('videoplayer-current-time')));
