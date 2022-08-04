a88cf911aa2d44a14aeb91f61cc9bc899187457b
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-08-04 11:54:30 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  feetbackForm: document.querySelector('.feedback-form'),
+  textArea: document.querySelector('textarea'),
+  emailArea: document.querySelector('input'),
+};
+refs.feetbackForm.addEventListener('input', throttle(inputTextArea, 500));
+refs.feetbackForm.addEventListener('submit', onSubmitButtonClick);
 
+const SAVE_USER_DATA_KEY = 'feedback-form-state';
+const userData = {};
+populateText();
+
+function inputTextArea(e) {
+  userData[e.target.name] = e.target.value;
+  localStorage.setItem(SAVE_USER_DATA_KEY, JSON.stringify(userData));
+}
+
+function onSubmitButtonClick(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(SAVE_USER_DATA_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(SAVE_USER_DATA_KEY);
+}
+
+function populateText() {
+  const saveData = JSON.parse(localStorage.getItem(SAVE_USER_DATA_KEY));
+  if (saveData) {
+    refs.textArea.value = saveData['message'];
+    refs.emailArea.value = saveData['email'];
+  }
+}
